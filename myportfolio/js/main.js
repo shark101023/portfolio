08760e7519f2869a1fbb2bdb5f9f6967185fc900
shark101023 <blueberrydrops1010@gmail.com>
2021-02-25 19:37:41 +0900

    アニメーションん実装

--- myportfolio/js/main.js
+++ myportfolio/js/main.js
@@ -7,29 +7,39 @@ ityped.init('#ityped', {
   startDelay: 500
 });
 
-// ボタンの表示／非表示を切り替える関数
+// ページトップボタン
 const updateButton = () => {
   if ($(window).scrollTop() >= 300) {
-    // 300px以上スクロールされた
-    // ボタンを表示
+ 
     $('.back-to-top').fadeIn();
   } else {
-    // ボタンを非表示
+  
     $('.back-to-top').fadeOut();
   }
 };
 
-// スクロールされる度にupdateButtonを実行
 $(window).on('scroll', updateButton);
 
-// ボタンをクリックしたらページトップにスクロールする
 $('.back-to-top').on('click', (e) => {
-  // ボタンのhrefに遷移しない
+ 
   e.preventDefault();
 
-  // 600ミリ秒かけてトップに戻る
   $('html, body').animate({ scrollTop: 0 }, 600);
 });
 
-// ページの途中でリロード（再読み込み）された場合でも、ボタンが表示されるようにする
 updateButton();
+
+// animate.css
+$('.animate__animated').waypoint({
+  handler(direction) {
+
+    if (direction === 'down') {
+      
+      $(this.element).addClass('animate__fadeInUp');
+
+      this.destroy();
+    }
+  },
+
+  offset: '100%',
+});
