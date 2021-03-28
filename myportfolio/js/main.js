9dc265622a4c9409fa4a032a861f47113c43cc0c
shark101023 <blueberrydrops1010@gmail.com>
2021-03-28 18:10:30 +0900

    Works追加

--- myportfolio/js/main.js
+++ myportfolio/js/main.js
@@ -1,45 +1,40 @@
 // ityped
-ityped.init('#ityped', {
+ityped.init("#ityped", {
   loop: false,
-  strings: ['Welcome to my portfolio!'],
+  strings: ["Welcome to my portfolio!"],
   typeSpeed: 100,
   backSpeed: 80,
-  startDelay: 500
+  startDelay: 500,
 });
 
 // ページトップボタン
 const updateButton = () => {
   if ($(window).scrollTop() >= 300) {
- 
-    $('.back-to-top').fadeIn();
+    $(".back-to-top").fadeIn();
   } else {
-  
-    $('.back-to-top').fadeOut();
+    $(".back-to-top").fadeOut();
   }
 };
 
-$(window).on('scroll', updateButton);
+$(window).on("scroll", updateButton);
 
-$('.back-to-top').on('click', (e) => {
- 
+$(".back-to-top").on("click", (e) => {
   e.preventDefault();
 
-  $('html, body').animate({ scrollTop: 0 }, 600);
+  $("html, body").animate({ scrollTop: 0 }, 600);
 });
 
 updateButton();
 
 // animate.css
-$('.animate__animated').waypoint({
+$(".animate__animated").waypoint({
   handler(direction) {
-
-    if (direction === 'down') {
-      
-      $(this.element).addClass('animate__fadeInUp');
+    if (direction === "down") {
+      $(this.element).addClass("animate__fadeInUp");
 
       this.destroy();
     }
   },
 
-  offset: '100%',
+  offset: "100%",
 });
